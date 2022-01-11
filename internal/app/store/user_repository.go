992576b043ed7aa9d9a819bada8f12c652c4d63b
Alexey Trapeznikov <alekceytr@mail.ru>
2022-01-11 22:57:25 +0800

    test findByEmail

--- internal/app/store/user_repository.go
+++ internal/app/store/user_repository.go
@@ -11,12 +11,17 @@ func (u *UserRepository) Create(m *model.User) (*model.User, error) {
 		"INSERT INTO users (email, encrypted_password) VALUES ($1, $2) RETURNING id",
 		m.Email,
 		m.EncryptedPassword,
-		).Scan(&m.ID); err != nil {
+	).Scan(&m.ID); err != nil {
 		return nil, err
 	}
 	return m, nil
 }
 
-func (u *UserRepository) FindByEmail(email string) (*model.User, error) {
-	return nil, nil
+func (ur *UserRepository) FindByEmail(email string) (*model.User, error) {
+	u := &model.User{}
+	if err := ur.store.db.QueryRow(
+		"SELECT id, email, encrypted_password FROM users WHERE email = $1", email).Scan(&u.ID, &u.Email, &u.EncryptedPassword); err != nil {
+		return nil, err
+	}
+	return u, nil
 }
--- internal/app/store/user_repository_test.go
+++ internal/app/store/user_repository_test.go
@@ -20,3 +20,19 @@ func TestUserRepository_Create(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
+
+func TestUserRepository_FindByEmail(t *testing.T) {
+	s, teardown := store.TestStore(t, databaseURL)
+	defer teardown("users")
+
+	email := "user@mail.com"
+	_, err := s.User().FindByEmail(email)
+	assert.Error(t, err)
+
+	s.User().Create(&model.User{
+		Email: "user@mail.com",
+	})
+	u, err := s.User().FindByEmail(email)
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
